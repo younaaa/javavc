1c228bb7b07a110b6a38596b866c1866c638003f
김우영 <wooyoung9425@nate.com>
2018-02-28 14:00:24 +0900

    4

--- 0228_step13_gittest/src/com/git4/Test6.java
+++ 0228_step13_gittest/src/com/git4/Test6.java
@@ -4,7 +4,7 @@ public class Test6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("윤아야 이번엔 되야되 ㅠㅠ");
+		System.out.println("윤아야 이번엔 되야되 ㅠㅠm");
 	}
 
 }
